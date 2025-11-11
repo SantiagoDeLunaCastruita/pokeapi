30a77df024a8b0d6c407e264b9b395bd74654a17
SantiagoDeLunaCastruita <24308060610637.cetis61.edu.mx>
2025-11-11 09:57:14 -0500

    fsv

--- app2.py
+++ app2.py
@@ -9,7 +9,13 @@ def index():
 @app.route('/search',methods=['POST'])
 def search_pokemon():
     pokemon_name = request.form.get('pokemon_name,').strip().lower()
-    
+    if not pokemon_name:
+        flash('porfavor,mo te ags wey','error')
+        return redirect(url_for('index'))
+    resp = request.get(f"{API}{pokemon_name}")
+    if resp.status_code == 200:
+        pokemon_data = resp.json()
+        return render_template('pokemon.html',pokemon=pokemon_name)
 
 
 if __name__ == '__main__':
